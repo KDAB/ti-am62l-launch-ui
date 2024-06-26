07552e5192ab301f9cf064923ce5343687d8467d
Marco Thaller <marco.thaller@kdab.com>
2024-06-26 17:02:55 +0200

    Benchmark ITR-06: 2x animated rects, 40x40, horizontal move in 5 sec
    Change-Id: I0af643cc16cb99b7ba67a2ae7cfbe29d5dc1f609

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
+    let ui_handle = ui.as_weak();
+
+    slint::Timer::single_shot(std::time::Duration::from_secs(5), move || {
+        ui_handle.unwrap().set_move_rectangles(true);
+    });
 
     ui.run()
 }
